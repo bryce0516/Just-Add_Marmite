f65561f9e66c14d3e7cd69088c4e3e2c9fe56217
Hyesung Park <51251258+bryce0516@users.noreply.github.com>
2021-05-03 10:53:40 +0900

    이미지적용

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -1,21 +1,27 @@
 import Link from "next/link";
-
+import Image from "next/image";
 const RecipeCard = ({ item }) => {
-  console.log(item);
-
   const { title, slug, cookingTime, thumbnail } = item.fields;
-
+  console.log(thumbnail);
   return (
     <div key={item.sys.id}>
       <div className="card">
-        <div className="featured"></div>
+        <div className="featured">
+          <Image
+            src={"https:" + thumbnail.fields.file.url}
+            width={thumbnail.fields.file.details.image.width}
+            height={thumbnail.fields.file.details.image.height}
+          />
+        </div>
         <div className="content">
           <div className="info">
             <h4>{title}</h4>
             <p>takes approx {cookingTime} mins to make</p>
           </div>
           <div className="actions">
-            <Link href={"/recipes/"}></Link>
+            <Link href={"/recipes/" + slug}>
+              <a>Cook this</a>
+            </Link>
           </div>
         </div>
       </div>
